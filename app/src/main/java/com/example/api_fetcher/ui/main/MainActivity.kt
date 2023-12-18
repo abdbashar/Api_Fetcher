3762fd61f99d79de584fb5ba93b9ff9a2e5f0df4
abdbashar <abdullahbashar98@gmail.com>
2023-12-18 19:52:54 +0300

    Added AndroidEntryPoint annotation to MainActivity

--- app/src/main/java/com/example/api_fetcher/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/api_fetcher/ui/main/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.example.api_fetcher
+package com.example.api_fetcher.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.api_fetcher.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
